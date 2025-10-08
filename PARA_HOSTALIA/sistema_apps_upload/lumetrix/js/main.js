368bf2fe97d30d5c3da268f293ab1ef3cd5ada77
anabguer <agl0305@gmail.com>
2025-10-08 16:53:30 +0200

    Fix borde animado: cambiar a ::after con overflow visible y blur

--- PARA_HOSTALIA/sistema_apps_upload/lumetrix/js/main.js
+++ PARA_HOSTALIA/sistema_apps_upload/lumetrix/js/main.js
@@ -1,4 +1,4 @@
-import { mount } from './game.bundle.js?v=20251008-1630'
+import { mount } from './game.bundle.js?v=20251008-1645'
 
 const root = document.getElementById('gameRoot')
 if (root && typeof mount === 'function') {
